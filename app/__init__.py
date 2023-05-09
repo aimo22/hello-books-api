9e630e19cdd8ad8e82ca69808d2a6297dea3d04c
Aisha <amdahir1@gmail.com>
2023-05-09 12:58:28 -0500

    testing changes

--- app/__init__.py
+++ app/__init__.py
@@ -12,22 +12,22 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # if not test_config:
+    if not test_config:
         # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         #     "SQLALCHEMY_DATABASE_URI")
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("RENDER_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("RENDER_DATABASE_URI")
 
-    # else:
-    #     app.config["TESTING"] = True
-    #     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    #         "SQLALCHEMY_TEST_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
         
     
 
 
     # Import models here
-    # from app.models.book import Book
-    # from app.models.author import Author
+    from app.models.book import Book
+    from app.models.author import Author
 
     db.init_app(app)
     migrate.init_app(app, db)
